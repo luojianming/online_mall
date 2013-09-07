248a91e8f01d0f066d9b7aa07b2aaeffff06d120
luojianming <luojmfly@gmail.com>
2013-09-07 12:03:08 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OnlineMall::Application.config.secret_token = '1308bb584daf2270a71ab16c0a4a149aa7b53d6e57a595953c759c7c1f47154f52686a8091d17ca668bb98e93ff415aaf8d136aec278297392834b9ee40966c6'
